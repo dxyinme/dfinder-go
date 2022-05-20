5c7f435f850a955b81f8fbf67e848a24c6834948
dxyinme <dxyinme@outlook.com>
2022-05-20 22:16:32 +0800

    update flag

--- test/register_test_main/register_test_main.go
+++ test/register_test_main/register_test_main.go
@@ -9,9 +9,10 @@ import (
 
 func main() {
 	addr := flag.String("addr", "0.0.0.2", "addr")
+	etcd_addr := flag.String("etcd_addr", "127.0.0.1:2379", "etcd addr")
 	flag.Parse()
 	etcd_conf := discover.DefaultEtcdCfg
-	etcd_conf.Endpoints = []string{"127.0.0.1:2379"}
+	etcd_conf.Endpoints = []string{*etcd_addr}
 	r, err := discover.NewRegisterWithEtcdCfg("testname1", *addr, "dev", etcd_conf)
 	if err != nil {
 		logrus.Error(err)
